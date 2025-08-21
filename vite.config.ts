0ea4b865c9253d04839d955bbeb4240bc866bbe1
benedethbillion-bit <benedethbillion@gmail.com>
2025-08-21 12:22:49 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/peace-pulse-co/',  // base path for GitHub Pages
+  plugins: [react()],
+  build: {
+    outDir: 'dist',          // ensure output goes to 'dist' folder
+    emptyOutDir: true        // clears previous builds
+  }
+})
